ac0a389710ff6d3261b6363babbe58e4dc1ca5f5
Marcos Kilmer <marcos.kilmer@foursys.com.br>
2021-08-08 07:44:05 -0300

    Initial Commit

---
+++ SpotifyVIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyVIP
+//
+//  Created by Marcos Kilmer Pereira de Aquino on 08/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpotifyVIPTests/SpotifyVIPTests.swift
@@ -0,0 +1,33 @@
+//
+//  SpotifyVIPTests.swift
+//  SpotifyVIPTests
+//
+//  Created by Marcos Kilmer Pereira de Aquino on 08/08/21.
+//
+
+import XCTest
+@testable import SpotifyVIP
+
+class SpotifyVIPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
